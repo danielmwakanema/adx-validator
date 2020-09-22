4f8f755a518f5abaa04834febb55b987afda9e35
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-09-22 09:12:55 +0200

    chore(App): register config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import DatabaseConfig from 'src/config/database.config';
+import ValidatorQueueConfig from 'src/config/validator-queue.config';
+import FailQueueConfig from 'src/config/fail-queue.config';
+import SuccessQueueConfig from 'src/config/success-queue.config';
+import AdxPayloadConfig from 'src/config/adx-payload.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ load: [DatabaseConfig, ValidatorQueueConfig, FailQueueConfig, SuccessQueueConfig, AdxPayloadConfig], isGlobal: true })
+  ]
 })
-export class AppModule {}
+export class AppModule { }
