ae6dc2314e1a081681318f10e29fa8bdc4548564
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-09-30 10:31:52 +0200

    refactor(App): turn into microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices/enums/transport.enum';
+import { MicroserviceOptions } from '@nestjs/microservices/interfaces/microservice-configuration.interface';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const service = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.VQ_HOST],
+      queue: process.env.VQ_NAME,
+      noAck: false,
+      queueOptions: {
+        durable: true
+      },
+    },
+  });
+  service.listen(() => console.log('Validation service is now listening...'));
 }
+
 bootstrap();
